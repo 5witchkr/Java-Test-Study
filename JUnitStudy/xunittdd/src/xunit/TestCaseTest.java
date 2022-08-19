aba7f024712c6ccb06a09e3a29e78077773bac84
5witchkr <hob4419@gmail.com>
2022-08-20 00:12:23 +0900

    feat: add testSetUp Mehthod

--- JUnitStudy/xunittdd/src/xunit/TestCaseTest.java
+++ JUnitStudy/xunittdd/src/xunit/TestCaseTest.java
@@ -1,14 +1,23 @@
 package xunit;
 
+
+//테스트케이스를 테스트하는 테스트
 public class TestCaseTest extends TestCase{
     public TestCaseTest(String name) {
         super(name);
     }
 
-    public void testRunning() {
+   public void testRunning() {
         WasRun wasRun = new WasRun("testMethod");
         Assert.assertEquals(false, wasRun.wasRun);
         wasRun.run();
         Assert.assertEquals(true, wasRun.wasRun);
+    }
+
+    public void testSetUp() {
+        WasRun wasRun = new WasRun("testMethod");
+        Assert.assertEquals(false, wasRun.wasSetUp);
+        wasRun.run();
+        Assert.assertEquals(true, wasRun.wasSetUp);
     }
 }
--- JUnitStudy/xunittdd/src/xunit/XUnitTest.java
+++ JUnitStudy/xunittdd/src/xunit/XUnitTest.java
@@ -2,7 +2,10 @@ package xunit;
 
 public class XUnitTest {
     public static void main(String[] args) {
-
+        //테스트 메소드 실행
         new TestCaseTest("testRunning").run();
+
+        //테스트 SetUp
+        new TestCaseTest("testSetUp").run();
     }
 }
