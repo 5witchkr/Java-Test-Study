139a88ba7da11a1e4f951e8425965cc41441904f
5witchkr <hob4419@gmail.com>
2022-08-21 22:21:06 +0900

    feat: testSuite

--- JUnitStudy/xunittdd/src/xunit/TestCase.java
+++ JUnitStudy/xunittdd/src/xunit/TestCase.java
@@ -10,8 +10,7 @@ public abstract class TestCase {
         this.name = name;
     }
 
-    public TestResult run() {
-        TestResult result = new TestResult();
+    public void run(TestResult result) {
         result.testStarted();
 
         //선실행
@@ -25,7 +24,6 @@ public abstract class TestCase {
         }
 
         tearDown();
-        return result;
     }
 
     public void tearDown() {
--- JUnitStudy/xunittdd/src/xunit/XUnitTest.java
+++ JUnitStudy/xunittdd/src/xunit/XUnitTest.java
@@ -2,14 +2,18 @@ package xunit;
 
 public class XUnitTest {
     public static void main(String[] args) {
+        TestResult result = new TestResult();
         //테스트 메소드 실행
-        new TestCaseTest("testTemplateMethod").run();
+        new TestCaseTest("testTemplateMethod").run(result);
         //result 테스트 실행
-        new TestCaseTest("testResult").run();
+        new TestCaseTest("testResult").run(result);
         //failed result formmating test
-        new TestCaseTest("testFailedResultFormatting").run();
+        new TestCaseTest("testFailedResultFormatting").run(result);
         //failed result test
-        new TestCaseTest("testFailedResult").run();
+        new TestCaseTest("testFailedResult").run(result);
+        //
+        new TestCaseTest("testSuite").run(result);
 
+        System.out.println(result.getSummary());
     }
 }
