ba0805ce4d0302020e70302d6f35c0b5be7d87d5
5witchkr <hob4419@gmail.com>
2022-08-23 13:36:19 +0900

    refactor: add TestCaseTest suite collection

--- JUnitStudy/xunittdd/src/xunit/TestCaseTest.java
+++ JUnitStudy/xunittdd/src/xunit/TestCaseTest.java
@@ -8,6 +8,22 @@ public class TestCaseTest extends TestCase{
     }
     //refactor
     WasRun wasRun;
+
+    public static TestSuite suite() {
+        TestSuite suite = new TestSuite();
+        //테스트 메소드 실행
+        suite.add(new TestCaseTest("testTemplateMethod"));
+        //result 테스트 실행
+        suite.add(new TestCaseTest("testResult"));
+        //failed result formmating test
+        suite.add(new TestCaseTest("testFailedResultFormatting"));
+        //failed result test
+        suite.add(new TestCaseTest("testFailedResult"));
+        //suite
+        suite.add(new TestCaseTest("testSuite"));
+        return suite;
+    }
+
     @Override
     public void setUp() {
         wasRun = new WasRun("testMethod");
--- JUnitStudy/xunittdd/src/xunit/XUnitTest.java
+++ JUnitStudy/xunittdd/src/xunit/XUnitTest.java
@@ -2,17 +2,8 @@ package xunit;
 
 public class XUnitTest {
     public static void main(String[] args) {
-        TestSuite suite = new TestSuite();
-        //테스트 메소드 실행
-        suite.add(new TestCaseTest("testTemplateMethod"));
-        //result 테스트 실행
-        suite.add(new TestCaseTest("testResult"));
-        //failed result formmating test
-        suite.add(new TestCaseTest("testFailedResultFormatting"));
-        //failed result test
-        suite.add(new TestCaseTest("testFailedResult"));
-        //suite
-        suite.add(new TestCaseTest("testSuite"));
+        //runner
+        TestSuite suite = TestCaseTest.suite();
         //run
         TestResult result = new TestResult();
         suite.run(result);
