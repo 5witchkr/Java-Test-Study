7551597f2e5de39e0672c88bee885e8001826b80
5witchkr <hob4419@gmail.com>
2022-08-28 21:40:15 +0900

    generic type erasure

--- supertypetoken/src/main/java/com/example/supertypetoken/TypeToken.java
+++ supertypetoken/src/main/java/com/example/supertypetoken/TypeToken.java
@@ -4,9 +4,20 @@ public class TypeToken {
     static <T> T create(Class<T> clazz) throws Exception {
         return clazz.newInstance();
     }
+    static class Generic<T> {
+        T value;
+        void set(T t) {}
+        T get() {return null;}
+    }//type erasure
 
     public static void main(String[] args) throws Exception {
         String o = create(String.class);
         System.out.println(o.getClass());
+
+        Generic<String> s = new Generic<String>();
+        s.value = "String~!";   //type erasure-> runtime => ObjectType
+        Generic<Integer> i = new Generic<Integer>();
+        i.value = 1;
+        i.set(10);
     }
 }
