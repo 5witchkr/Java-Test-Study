8adca4440d1b0758ee028ffc84d15e38ca891de8
5witchkr <hob4419@gmail.com>
2022-08-28 21:54:13 +0900

    safetype cast

--- supertypetoken/src/main/java/com/example/supertypetoken/TypeToken.java
+++ supertypetoken/src/main/java/com/example/supertypetoken/TypeToken.java
@@ -9,6 +9,9 @@ public class TypeToken {
         <T> void put(Class<T> clazz, T value) {
             map.put(clazz, value);
         }
+        <T> T get(Class<T> clazz){
+            return clazz.cast(map.get(clazz));  //.cast -> 명시적 타입캐스팅
+        }
     }
 
     public static void main(String[] args) throws Exception {
