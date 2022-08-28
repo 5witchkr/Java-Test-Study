bc3d7b88f34f5cd99f8d94a984715e3310ff2145
5witchkr <hob4419@gmail.com>
2022-08-28 21:53:03 +0900

    typesafeMap

--- supertypetoken/src/main/java/com/example/supertypetoken/TypeToken.java
+++ supertypetoken/src/main/java/com/example/supertypetoken/TypeToken.java
@@ -1,23 +1,19 @@
 package com.example.supertypetoken;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TypeToken {
-    static <T> T create(Class<T> clazz) throws Exception {
-        return clazz.newInstance();
+    static class TypesafeMap {
+        Map<Class<?>, Object> map = new HashMap<>();
+        <T> void put(Class<T> clazz, T value) {
+            map.put(clazz, value);
+        }
     }
-    static class Generic<T> {
-        T value;
-        void set(T t) {}
-        T get() {return null;}
-    }//type erasure
 
     public static void main(String[] args) throws Exception {
-        String o = create(String.class);
-        System.out.println(o.getClass());
-
-        Generic<String> s = new Generic<String>();
-        s.value = "String~!";   //type erasure-> runtime => ObjectType
-        Generic<Integer> i = new Generic<Integer>();
-        i.value = 1;
-        i.set(10);
+        TypesafeMap m = new TypesafeMap();
+        m.put(Integer.class, 1);
+        m.put(String.class, "string");
     }
 }
