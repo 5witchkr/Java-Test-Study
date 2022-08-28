f9520b07228d772497512d4d22852363a7c15cd3
5witchkr <hob4419@gmail.com>
2022-08-28 21:33:22 +0900

    generic created instance

---
+++ supertypetoken/src/main/java/com/example/supertypetoken/TypeToken.java
@@ -0,0 +1,12 @@
+package com.example.supertypetoken;
+
+public class TypeToken {
+    static <T> T create(Class<T> clazz) throws Exception {
+        return clazz.newInstance();
+    }
+
+    public static void main(String[] args) throws Exception {
+        String o = create(String.class);
+        System.out.println(o.getClass());
+    }
+}
