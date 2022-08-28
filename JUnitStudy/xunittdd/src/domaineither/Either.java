ae2cb92f8a9a8f96a4250fe297d6af2b371e0bfd
5witchkr <hob4419@gmail.com>
2022-08-28 20:55:18 +0900

    주석처리

--- JUnitStudy/xunittdd/src/domaineither/Either.java
+++ JUnitStudy/xunittdd/src/domaineither/Either.java
@@ -1,11 +1,11 @@
 package domaineither;
 
-public class Either<DomainResult, RuntimeException> {
-    getDomainResult(DomainParam params){
-        Either<DomainResult, RuntimeException> result = getResult(params)
-                .right()
-                .map(DomainResult::fromEntity)
-                .toEither();
-        return result;
-    }
-}
+//public class Either<DomainResult, RuntimeException> {
+//    getDomainResult(DomainParam params){
+//        Either<DomainResult, RuntimeException> result = getResult(params)
+//                .right()
+//                .map(DomainResult::fromEntity)
+//                .toEither();
+//        return result;
+//    }
+//}
--- JUnitStudy/xunittdd/src/domaineither/EitherSupport.java
+++ JUnitStudy/xunittdd/src/domaineither/EitherSupport.java
@@ -5,35 +5,35 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public interface EitherSupport {
-    default <P, R> Either<? extends RuntimeException, R>
-    getResult(Function<P, R> func, P param, Logger logger) {
-        try {
-            return Either.right(func.apply(param));
-        } catch (RuntimeException re) {
-            return Either.left(re);
-        }
-    }
-
-    default <P, R> Either<? extends RuntimeException, List<R>>
-    getResultList(Function<P, List<R>> func, P param, Logger logger) {
-        try {
-            return Either.right(runc.apply(param));
-        } catch (RuntimeException re) {
-            System.out.println("Error occurred");
-            return Either.left(re);
-        }
-    }
-
-    @Slf4j
-    public class DomainCode implements EitherSupport {
-        public void f() {
-            Either<? extends RuntimeException, String> resultOrException = getResult(String::valueOf,0,log);
-            Either<? extends RuntimeException, List<String>> resultListOrException = getResultList(
-                    (Integer i) -> IntStream.range(0, i)
-                            .boxed()
-                            .map(String::valueOf)
-                            .collect(Collectors.toList()), 0 , log);
-        }
-    }
-}
+//public interface EitherSupport {
+//    default <P, R> Either<? extends RuntimeException, R>
+//    getResult(Function<P, R> func, P param, Logger logger) {
+//        try {
+//            return Either.right(func.apply(param));
+//        } catch (RuntimeException re) {
+//            return Either.left(re);
+//        }
+//    }
+//
+//    default <P, R> Either<? extends RuntimeException, List<R>>
+//    getResultList(Function<P, List<R>> func, P param, Logger logger) {
+//        try {
+//            return Either.right(runc.apply(param));
+//        } catch (RuntimeException re) {
+//            System.out.println("Error occurred");
+//            return Either.left(re);
+//        }
+//    }
+//
+//    @Slf4j
+//    public class DomainCode implements EitherSupport {
+//        public void f() {
+//            Either<? extends RuntimeException, String> resultOrException = getResult(String::valueOf,0,log);
+//            Either<? extends RuntimeException, List<String>> resultListOrException = getResultList(
+//                    (Integer i) -> IntStream.range(0, i)
+//                            .boxed()
+//                            .map(String::valueOf)
+//                            .collect(Collectors.toList()), 0 , log);
+//        }
+//    }
+//}
