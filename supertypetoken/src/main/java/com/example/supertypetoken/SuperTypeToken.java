f0031181673f2895084d060b725200b52969db2c
5witchkr <hob4419@gmail.com>
2022-08-30 01:37:44 +0900

    setup: superTypeToken

--- supertypetoken/src/main/java/com/example/supertypetoken/SuperTypeToken.java
+++ supertypetoken/src/main/java/com/example/supertypetoken/SuperTypeToken.java
@@ -2,18 +2,30 @@ package com.example.supertypetoken;
 
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class SuperTypeToken {
-    static class Sup<T> {
-        T value;
+    static class TypesafeMap {
+        Map<Class<?>, Object> map = new HashMap<>();
+        <T> void put(Class<T> clazz, T value){
+            map.put(clazz, value);
+        }
+        <T> T get(Class<T> clazz) {
+            return clazz.cast(map.get(clazz));
+        }
     }
 
-    public static void main(String[] args) throws NoSuchFieldException{
-        //anonymous class
-        Sup b = new Sup<String>(){};
-        //not eraser (runtime type safe)
-        Type t = b.getClass().getGenericSuperclass();
-        ParameterizedType ptype = (ParameterizedType) t;
-        System.out.println(ptype.getActualTypeArguments()[0]);
+    public static void main(String[] args) throws Exception{
+        TypesafeMap m = new TypesafeMap();
+        m.put(Integer.class, 1);
+        m.put(String.class, "String");
+        m.put(List.class, Arrays.asList(1,2,3));
+
+        System.out.println(m.get(Integer.class));
+        System.out.println(m.get(String.class));
+        System.out.println(m.get(List.class));
     }
 }
