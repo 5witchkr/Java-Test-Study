9a5408b4b94b07888b07bbf01f135761fb62bff5
5witchkr <hob4419@gmail.com>
2022-09-02 00:40:37 +0900

    docs: generic base

--- generic/src/main/java/com/example/generic/Generics.java
+++ generic/src/main/java/com/example/generic/Generics.java
@@ -1,4 +1,18 @@
 package com.example.generic;
 
 public class Generics {
+    static class Hello<T> {//T -> type parameter
+        T t;
+        T method(T val){return null;}
+    }
+
+    static void print(String value){
+        System.out.println(value);
+    }
+
+    public static void main (String[] args){
+        print("Generic");
+        new Hello<String>(); //type argument
+    }
 }
+//for typesafe, for recycle generic code
