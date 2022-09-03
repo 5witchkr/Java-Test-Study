b696f4a862f10ead733e8e06d483ad546abd4350
5witchkr <hob4419@gmail.com>
2022-09-03 22:42:01 +0900

    feat: genetic ex

--- generic/src/main/java/com/example/generic/Generics.java
+++ generic/src/main/java/com/example/generic/Generics.java
@@ -1,18 +1,16 @@
 package com.example.generic;
 
-public class Generics {
-    static class Hello<T> {//T -> type parameter
-        T t;
-        T method(T val){return null;}
-    }
+import java.util.ArrayList;
+import java.util.List;
 
-    static void print(String value){
-        System.out.println(value);
+public class Generics {
+    <T> void print(T t){
+        System.out.println(t.toString());
     }
 
     public static void main (String[] args){
-        print("Generic");
-        new Hello<String>(); //type argument
+        new Generics().print("hello");
+        new Generics().print(1);
     }
 }
 //for typesafe, for recycle generic code
