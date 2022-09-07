2cec0f95bd012954a9176d3d50bcc999028ab646
5witchkr <hob4419@gmail.com>
2022-09-08 00:29:40 +0900

    feat: executor

--- reactivestream/src/main/java/com/example/reactivestream/PubSub.java
+++ reactivestream/src/main/java/com/example/reactivestream/PubSub.java
@@ -2,14 +2,15 @@ package com.example.reactivestream;
 
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.concurrent.*;
 import java.util.concurrent.Flow.Subscriber;
 import java.util.concurrent.Flow.Publisher;
-import java.util.concurrent.Flow;
 import java.util.concurrent.Flow.Subscription;
 
 public class PubSub {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Iterable<Integer> itr = Arrays.asList(1,2,3,4,5);
+        ExecutorService es = Executors.newCachedThreadPool();
 
         Publisher p = new Publisher() {
           @Override
@@ -18,18 +19,21 @@ public class PubSub {
               subscriber.onSubscribe(new Subscription() {
                   @Override
                   public void request(long n) {
-                      try {
-                          while (n-- > 0){
-                              if (it.hasNext()){
-                                  subscriber.onNext(it.next());
-                              } else {
-                                  subscriber.onComplete();
-                                  break;
+                      es.execute(() -> {
+                          int i = 0;
+                          try {
+                              while (i++ < n){
+                                  if (it.hasNext()){
+                                      subscriber.onNext(it.next());
+                                  } else {
+                                      subscriber.onComplete();
+                                      break;
+                                  }
                               }
+                          } catch (RuntimeException e){
+                              subscriber.onError(e);
                           }
-                      } catch (RuntimeException e){
-                          subscriber.onError(e);
-                      }
+                      });
                   }
 
                   @Override
@@ -43,28 +47,30 @@ public class PubSub {
             Subscription subscription;
             @Override//need
             public void onSubscribe(Subscription subscription) {
-                System.out.println("onSubscribe");
+                System.out.println(Thread.currentThread().getName()+"onSubscribe");
                 this.subscription = subscription;
-                this.subscription.request(2);
+                this.subscription.request(1);
             }
 
             @Override//optional
             public void onNext(Integer item) {
-                System.out.println("onNext " + item);
+                System.out.println(Thread.currentThread().getName()+"onNext " + item);
                 this.subscription.request(1);
             }
 
             @Override//optional
             public void onError(Throwable throwable) {
-                System.out.println("onError");
+                System.out.println("onError"+throwable.getMessage());
             }
 
             @Override//optional
             public void onComplete() {
-                System.out.println("onComplete");
+                System.out.println(Thread.currentThread().getName()+"onComplete");
             }
         };
 
         p.subscribe(s);
+        es.awaitTermination(10, TimeUnit.HOURS);
+        es.shutdown();
     }
 }
