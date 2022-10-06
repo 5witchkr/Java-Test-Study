3c5f1142e26ec933909e1815ef2fbfa9811e8634
5witchkr <hob4419@gmail.com>
2022-10-07 00:12:49 +0900

    feat: reactor stream example case

--- reactivestream/src/main/java/com/example/reactivestream/ReactorEx.java
+++ reactivestream/src/main/java/com/example/reactivestream/ReactorEx.java
@@ -7,6 +7,21 @@ import reactor.core.publisher.Flux;
  */
 public class ReactorEx {
     public static void main(String[] args) {
+
+        //case 1
+        Flux.<Integer>create(e -> {
+                    e.next(1);
+                    e.next(2);
+                    e.next(3);
+                    e.complete();
+                })
+                .log()
+                .map(s->s*10)
+                .log()
+                .subscribe(System.out::println);
+
+
+        //case 2
         Flux.<Integer>create(e -> {
             e.next(1);
             e.next(2);
