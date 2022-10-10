01e5a22802c9ae8e3d7239b78c0f83e9aa752a8d
5witchkr <hob4419@gmail.com>
2022-10-10 23:37:19 +0900

    feat: init schedualerEx

---
+++ reactivestream/src/main/java/com/example/reactivestream/scheduler/SchedulerEx.java
@@ -0,0 +1,51 @@
+package com.example.reactivestream.scheduler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+
+@Slf4j
+public class SchedulerEx {
+    public static void main(String[] args) {
+        Publisher<Integer> publisher = sub -> {
+            sub.onSubscribe(new Subscription() {
+                @Override
+                public void request(long n) {
+                    sub.onNext(1);
+                    sub.onNext(2);
+                    sub.onNext(3);
+                    sub.onComplete();
+                }
+
+                @Override
+                public void cancel() {
+
+                }
+            });
+        };
+        publisher.subscribe(new Subscriber<Integer>() {
+            @Override
+            public void onSubscribe(Subscription s) {
+                log.debug("onSubscribe");
+                s.request(Long.MAX_VALUE);
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                log.debug("onNext: {}",integer);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                log.debug("onError: {}",t);
+            }
+
+            @Override
+            public void onComplete() {
+                log.debug("onComplete");
+            }
+        });
+    }
+}
