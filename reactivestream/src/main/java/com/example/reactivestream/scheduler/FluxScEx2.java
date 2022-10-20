d4fdafb0588e736e9a99c5e03863b17fc6b33c9b
5witchkr <hob4419@gmail.com>
2022-10-21 00:51:28 +0900

    feat: intervalEx log

--- reactivestream/src/main/java/com/example/reactivestream/scheduler/FluxScEx2.java
+++ reactivestream/src/main/java/com/example/reactivestream/scheduler/FluxScEx2.java
@@ -13,7 +13,7 @@ public class FluxScEx2 {
     public static void main(String[] args) throws InterruptedException {
         //interval -> 주기를 가지고 무한대로 실행
         Flux.interval(Duration.ofMillis(500))
-                .subscribe(System.out::println);
+                .subscribe(s-> log.debug("onNext:{}", s));
 
         log.debug("exit");
         TimeUnit.SECONDS.sleep(5);
