ce70b8066e1d0136efb6210acf986c046c92a412
5witchkr <hob4419@gmail.com>
2022-10-21 00:58:38 +0900

    feat: userThread Case

--- reactivestream/src/main/java/com/example/reactivestream/scheduler/FluxScEx2.java
+++ reactivestream/src/main/java/com/example/reactivestream/scheduler/FluxScEx2.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 
 import java.time.Duration;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 
@@ -11,11 +12,22 @@ import java.util.concurrent.TimeUnit;
 @Slf4j
 public class FluxScEx2 {
     public static void main(String[] args) throws InterruptedException {
-        //interval -> 주기를 가지고 무한대로 실행
-        Flux.interval(Duration.ofMillis(500))
-                .subscribe(s-> log.debug("onNext:{}", s));
+//        //interval -> 주기를 가지고 무한대로 실행
+//        Flux.interval(Duration.ofMillis(500))
+//                .subscribe(s-> log.debug("onNext:{}", s));
+//
+//        log.debug("exit");
+//        TimeUnit.SECONDS.sleep(5);
 
-        log.debug("exit");
-        TimeUnit.SECONDS.sleep(5);
+
+        //otherCase userThread는 mainThread가 끝나도 계속됨 (JVM이 계속 떠있다.)
+        Executors.newSingleThreadExecutor().execute(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+            }
+            System.out.println("Hello");
+        });
+        System.out.println("Exit");
     }
 }
