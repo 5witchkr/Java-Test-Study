8b97b752bb76e5c9f697675adf67df1372a56568
5witchkr <hob4419@gmail.com>
2022-10-21 00:49:41 +0900

    feat: init FluxEx2 interval

---
+++ reactivestream/src/main/java/com/example/reactivestream/scheduler/FluxScEx2.java
@@ -0,0 +1,21 @@
+package com.example.reactivestream.scheduler;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+
+
+@Slf4j
+public class FluxScEx2 {
+    public static void main(String[] args) throws InterruptedException {
+        //interval -> 주기를 가지고 무한대로 실행
+        Flux.interval(Duration.ofMillis(500))
+                .subscribe(System.out::println);
+
+        log.debug("exit");
+        TimeUnit.SECONDS.sleep(5);
+    }
+}
