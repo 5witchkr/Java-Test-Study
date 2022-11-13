636a860219b8140f60eaffe94390530139b861f2
5witchkr <hob4419@gmail.com>
2022-11-14 00:43:57 +0900

    feat: exception callback

--- reactivestream/src/main/java/com/example/reactivestream/future/FutureTaskEx.java
+++ reactivestream/src/main/java/com/example/reactivestream/future/FutureTaskEx.java
@@ -11,21 +11,28 @@ public class FutureTaskEx {
     interface SuccessCallback {
         void onSuccess(String result);
     }
+    interface ExceptionCallback {
+        void onError(Throwable t);
+    }
 
     public static class CallbackFutureTask extends FutureTask<String> {
         SuccessCallback sc;
-        public CallbackFutureTask(Callable<String> callable, SuccessCallback sc) {
+        ExceptionCallback ec;
+        public CallbackFutureTask(Callable<String> callable, SuccessCallback sc, ExceptionCallback ec) {
             super(callable);
             //null이면 nullpointexception
             this.sc = Objects.requireNonNull(sc);
+            this.ec = Objects.requireNonNull(ec);
         }
 
         @Override
         protected void done() {
             try {
                 sc.onSuccess(get());
-            } catch (InterruptedException | ExecutionException e) {
-                throw new RuntimeException(e);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            } catch (ExecutionException e) {
+                ec.onError(e.getCause());
             }
         }
     }
@@ -35,9 +42,12 @@ public class FutureTaskEx {
 
         CallbackFutureTask cf = new CallbackFutureTask(() -> {
             Thread.sleep(2000);
+            if (true) throw new RuntimeException("Async ERR");
             log.info("Async");
             return "Hello";
-        }, log::info);
+        },
+                r-> log.info("RESULT: " + r),
+                e -> log.info("ERROR: " + e.getMessage()));
 
         es.execute(cf);
         es.shutdown();
