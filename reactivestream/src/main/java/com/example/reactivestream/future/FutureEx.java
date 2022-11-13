e3e8a5dc0a6981fedb93b289895efedb16b9339c
5witchkr <hob4419@gmail.com>
2022-11-14 00:06:40 +0900

    feat: java future.get 의 blocking

--- reactivestream/src/main/java/com/example/reactivestream/future/FutureEx.java
+++ reactivestream/src/main/java/com/example/reactivestream/future/FutureEx.java
@@ -2,6 +2,7 @@ package com.example.reactivestream.future;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
@@ -9,18 +10,19 @@ import java.util.concurrent.Future;
 
 @Slf4j
 public class FutureEx {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         ExecutorService es = Executors.newCachedThreadPool();
 
-        es.execute(() -> {
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+        //Future = 미래에 실행되는 비동기작업의 결과물
+        //submit = return 값을 가질 수 있는 callable
+        Future<String> f = es.submit(() -> {
+            Thread.sleep(2000);
             log.info("Async");
+            return "Hello";
         });
 
+        //Future.get()은 결과가 올때까지 Blocking 됨
+        log.info(f.get());
         log.info("Exit");
     }
 }
